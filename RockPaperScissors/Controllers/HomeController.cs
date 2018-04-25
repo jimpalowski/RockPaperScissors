19e32314853e61596fdc934af08a68adcce6f39d
Guest User <Guest@epicodus-10.local>
2018-04-25 10:24:57 -0700

    Add css

--- RockPaperScissors/Controllers/HomeController.cs
+++ RockPaperScissors/Controllers/HomeController.cs
@@ -63,39 +63,4 @@ namespace RockPaperScissorController
     }
   }
 
-  //Get["/"] = _ => View["index.cshtml"];
-  //Get["/player1"] =_=> View["player1.cshtml"];
-  //Post["/p1Rock"] =_=> {
-    //string p1choice = "rock";
-    //return View["player2.cshtml", p1choice];
-  //};
-
-  // Post["/p1Paper"] =_=> {
-  //   string p1choice = "paper";
-  //   return View["player2.cshtml", p1choice];
-  // };
-
-  // Post["/p1Scissors"] =_=> {
-  //   string p1choice = "scissors";
-  //   return View["player2.cshtml", p1choice];
-  // };
-
-  // Post["/p2Rock"] =_=> {
-  //   string p1choice = Request.Form["p1choice"];
-  //   string p2choice = "rock";
-  //   Game game = new Game(p1choice, p2choice);
-  //   return View["result.cshtml", game.Play()];
-  // };
-
-  // Post["/p2Paper"] =_=> {
-  //   string p1choice = Request.Form["p1choice"];
-  //   string p2choice = "paper";
-  //   Game game = new Game(p1choice, p2choice);
-  //   return View["result.cshtml", game.Play()];
-  // };
-
-  // Post["/p2Scissors"] =_=> {
-  //   string p1choice = Request.Form["p1choice"];
-  //   string p2choice = "scissors";
-  //   Game game = new Game(p1choice, p2choice);
-  //   return View["result.cshtml", game.Play()];
+  
